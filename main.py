255836ee99bdfdc4e4cf932c0292b263871f0874
Jose Laurito <jigolaurito93@yahoo.com>
2023-08-26 03:40:40 -0500

    created a condition if response is espresso

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from menu import MENU, resources
 
+water = resources["water"]
+milk = resources["milk"]
+coffee = resources["coffee"]
+
+money = 0
+
 # Flag to indicate that coffee machine is off or on
 machine_off = False
 
 # 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-# response = input(f"What would you like? (espresso/latte/cappuccino): ").lower()
+response = input(f"What would you like? (espresso/latte/cappuccino): ").lower()
 # print(response)
 
 # 2. Turn off the Coffee Machine by entering “off” to the prompt.
@@ -20,14 +26,31 @@ def print_report(resources):
     water = resources["water"]
     milk = resources["milk"]
     coffee = resources["coffee"]
-    print(f"Water: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g")
+    print(f"\nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g")
 
 # If you type "report" as response, it will print the resource values
-# if response == "report":
-#     print_report(resources)
+if response == "report":
+    print_report(resources)
 
 
 # 4. Check resources sufficient?
+
+# Espresso
+espresso_water = MENU["espresso"]["ingredients"]["water"]
+espresso_coffee = MENU["espresso"]["ingredients"]["coffee"]
+
+if response == "espresso":
+    if espresso_water > water:
+        print(f"Sorry there is not enough water.")
+    elif espresso_water < water:
+        if espresso_coffee > coffee:
+            print(f"Sorry there is not enough water.")
+        elif espresso_coffee < coffee:
+            water -= espresso_water
+            coffee -= espresso_coffee
+
+print(f"Water {water}, Coffee {coffee}")
+
 
 # 5. Process coins.
 
